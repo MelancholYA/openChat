d476b181a244e37de8a2add39fe569b87a9689a0
yacineOuardi <ouardi.yacin3@gmail.com>
2022-04-10 01:16:07 +0100

    ws mess

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,31 @@ const userRoute = require('./routes/user');
 const chatRoute = require('./routes/chat');
 const { errorHandler } = require('./middlwares/errorMiddlware');
 const connectDB = require('./config/db');
+const app = express();
+const expressWs = require('express-ws')(app);
 
 connectDB();
 
-const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/auth', authRoute);
-app.use('/user', userRoute);
-app.use('/chat', chatRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/user', userRoute);
+app.use('/api/chat', chatRoute);
+
+// app.ws('/ws', function (wsi, req) {
+// 	wsi.on('connection', () => console.log('connected'));
+// 	wsi.on('message', function (msg) {
+// 		wsi.send('expressWs.getWss().clients');
+// 		console.log(expressWs.getWss().clients);
+// 	});
+// });
+expressWs.app.ws('/ws', (ws, req) => {
+	ws.on('message', function (msg) {
+		ws.send('expressWs.getWss().clients');
+		console.log(typeof expressWs.getWss().clients);
+	});
+});
 
 app.use(errorHandler);
 
